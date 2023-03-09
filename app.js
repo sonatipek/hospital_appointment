d94a5adb0aa070f235708529e45efd2deedc8927
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-03-09 10:24:35 +0300

    template engine added, routed with express.js

---
+++ app.js
@@ -0,0 +1,40 @@
+// MYSQL
+const mysql = require('mysql');
+const connection = require('./db/mysqlConenction'); //export msyq connection
+
+// Express
+const express = require('express');
+const app = express()
+const port = 3000
+const hostname = "localhost"
+
+app.set("view engine", "ejs")
+
+// Routing
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+app.get("/hastakayit", (req, res) => {
+    res.render("hastakayit.ejs")
+})
+app.get("/doktorkayit", (req, res) => {
+    res.render("doktorkayit.ejs")
+})
+app.get("/hastalist", (req, res) => {
+    res.render("hastalist.ejs")
+})
+app.get("/doktorlist", (req, res) => {
+    res.render("doktorlist.ejs")
+})
+app.get("/doktorgiris", (req, res) => {
+    res.render("doktorgiris.ejs")
+})
+app.get("/hastagiris", (req, res) => {
+    res.render("hastagiris.ejs")
+})
+
+
+
+app.listen(port,hostname, () =>{
+    console.log(`http://${hostname}:${port}`);
+})
