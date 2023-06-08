af2bc4cd9a7dab6a2820ee3fe544b886be009aba
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-06-08 10:09:50 +0300

    EJS added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // MYSQL
-const mysql = require('mysql');
-const connection = require('./db/mysqlConenction'); //export msyq connection
+// const mysql = require('mysql');
+const connection = require('./db/mysqlConenction'); //export mysql connection
 
 // Express
 const express = require('express');
@@ -8,8 +8,17 @@ const app = express()
 const port = 3000
 const hostname = "localhost"
 
+// Body Parser
+const bodyParser = require('body-parser');
+
+
 app.set("view engine", "ejs")
 app.use(express.static('images'));
+
+// Bodyparser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 
 // Routing
 app.get("/", (req, res) => {
@@ -39,9 +48,35 @@ app.get("/doktorlist", (req, res) => {
         }
     })
 })
-app.get("/doktorgiris", (req, res) => {
-    res.render("doktorgiris.ejs")
+app.get('/doktorgiris', (req, res) => {
+    res.render("doktorgiris", {basari: "nötr"})
+    console.log("get");
 })
+app.get("/doktorgiriskontrol", (req, res) => {
+    console.log("post");
+    connection.query('SELECT doktor_email FROM doktorlar', (err, results, fields)=>{
+        if (err){
+            res.send("<h1> Sunucu Tarafında bir hata oluştu </h1>" + err + "<br><a href='/'> Ana Menüye Dön </a>")
+        }else{
+            let requestMail = req.query.giris__doktormail
+
+            results.forEach(el => {
+                console.log(el.doktor_email, requestMail);
+                if (el.doktor_email === requestMail) {
+                    res.render("doktorgiris", {basari: true})
+                }
+                else{
+                    res.render("doktorgiris", {basari:false})
+                }
+            });
+            
+    
+        }
+    })
+    
+})
+
+
 app.get("/hastagiris", (req, res) => {
     res.render("hastagiris.ejs")
 })
